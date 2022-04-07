22191443dc1b13695a1bd1d83364ed2abd640d7f
Paris Walters <wparis@wparis-a01.vmware.com>
2022-04-07 08:13:03 -0400

    updated version

---
+++ aws_eks/backend.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_version = ">=1.1.0"
+
+  cloud {
+    organization = "VeloCloud-Terraform"
+
+    workspaces {
+      name = "apps-test-us-east-1"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.72.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = ">= 3.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = ">= 2.2"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
