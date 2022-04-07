51f40b8af69bb0e6c67820bbfaf15c53ab96c75e
Paris N Walters <pwalters2004@gmail.com>
2022-04-07 10:14:11 -0400

    Update main.tf

--- ec2/main.tf
+++ ec2/main.tf
@@ -28,7 +28,7 @@ module "ec2_instance" {
 
   name = "single-instance"
 
-  ami                    = "ami-b21122d8"
+  ami                    = "ami-0c02fb55956c7d316"
   instance_type          = "t2.micro"
   key_name               = "user1"
   monitoring             = true
