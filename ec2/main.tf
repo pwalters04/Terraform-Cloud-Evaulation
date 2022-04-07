e5f2c5ecd5cfdd450a40779f6bb869a37bb8b1b7
Paris N Walters <pwalters2004@gmail.com>
2022-04-07 10:18:34 -0400

    Update main.tf

--- ec2/main.tf
+++ ec2/main.tf
@@ -30,7 +30,6 @@ module "ec2_instance" {
 
   ami                    = "ami-0c02fb55956c7d316"
   instance_type          = "t2.micro"
-  key_name               = "user1"
   monitoring             = true
   vpc_security_group_ids = ["sg-03fa3d50ea179ad8f"]
   subnet_id              = "subnet-023aeca8a04af0af5"
