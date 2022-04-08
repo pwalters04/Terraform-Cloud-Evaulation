0367a94f065f63ab39633c9e1aa28023a974791c
Paris Walters <wparis@wparis-a01.vmware.com>
2022-04-08 15:32:37 -0400

    clean up code

--- aws_eks/vpc.tf
+++ aws_eks/vpc.tf
@@ -22,11 +22,8 @@ data "aws_subnet" "private" {
 }
 
 output "subnet_cidr_blocks" {
-  value = [for s in data.aws_subnet.private : s.]
+  value = [for s in data.aws_subnet.private : s.cidr_block]
 }
-
-
-
 
 output "vpc-id" {
   value = data.aws_vpc.vpc.id
