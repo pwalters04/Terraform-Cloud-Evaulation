e2addbdb290100eff67b16a2799b2add1c6db8c7
Paris Walters <wparis@wparis-a01.vmware.com>
2022-04-08 15:18:12 -0400

    figuring out subnets

--- aws_eks/vpc.tf
+++ aws_eks/vpc.tf
@@ -9,6 +9,28 @@ data "aws_vpc" "vpc"{
   }
 }
 
+data "aws_subnets" "private" {
+  filter {
+    name   = "tag:Type"
+    values = ["Private"]
+  }
+}
+
+data "aws_subnet" "private" {
+  for_each = toset(data.aws_subnets.private.ids)
+  id       = each.value
+}
+
+output "subnet_cidr_blocks" {
+  value = [for s in data.aws_subnet.private : s.]
+}
+
+
+
+
 output "vpc-id" {
   value = data.aws_vpc.vpc.id
+}
+output "subnet-info" {
+  value = data.aws_subnets.private.id
 }
